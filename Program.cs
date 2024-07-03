6097a7cf5ca22cd5b248308cb8b64282bd07d33e
Isaque <Isaque@DESKTOP-6IC7LO9>
2024-07-03 00:14:43 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testeTarget
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random faturamento = new Random();
+            int[] faturamentoAno = Enumerable.Range(0, 365).Select(_ => faturamento.Next(0, 10000)).Distinct().ToArray();
+            double mediaFaturamento = faturamentoAno.Where(ignoraZero => ignoraZero != 0).Average();
+            int maiorFaturamentoDia = faturamentoAno.Max();
+            int menorFaturamentoDia = faturamentoAno.Min();
+            int diasAcimaMedia = 0;
+            foreach (int dia in faturamentoAno)
+            {
+                if (dia > mediaFaturamento)
+                {
+                    diasAcimaMedia++;
+                }
+            }
+            Console.WriteLine($"Menor valor de faturamento ocorrido em um dia do ano: {menorFaturamentoDia}");
+            Console.WriteLine($"Maior valor de faturamento ocorrido em um dia do ano: {maiorFaturamentoDia}");
+            Console.WriteLine($"Número de dias no ano em que o valor de faturamento diário foi superior à média anual: {diasAcimaMedia}");
+        }
+
+
+
+    }
+}
